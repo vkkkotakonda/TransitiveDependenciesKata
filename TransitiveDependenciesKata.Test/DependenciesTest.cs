74ced6504b53cbb8c9d99c0067350d4e58908559
Grokodile <pnm@paulmackintosh.com>
2014-07-14 23:06:08 +0100

    A .sln for the kata.

---
+++ TransitiveDependenciesKata.Test/DependenciesTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace TransitiveDependenciesKata.Test
+{
+    [TestFixture]
+    public class DependenciesTest
+    {
+        Dependencies _dependencies;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dependencies = new Dependencies();
+            _dependencies.AddDirect("A", new[] { "B", "C" });
+            _dependencies.AddDirect("B", new[] { "C", "E" });
+            _dependencies.AddDirect("C", new[] { "G" });
+            _dependencies.AddDirect("D", new[] { "A", "F" });
+            _dependencies.AddDirect("E", new[] { "F" });
+            _dependencies.AddDirect("F", new[] { "H" });
+        }
+
+        [TestCase("A", new[] { "B", "C", "E", "F", "G", "H" })]
+        [TestCase("B", new[] { "C", "E", "F", "G", "H" })]
+        [TestCase("C", new[] { "G" })]
+        [TestCase("D", new[] { "A", "B", "C", "E", "F", "G", "H" })]
+        [TestCase("E", new[] { "F", "H" })]
+        [TestCase("F", new[] { "H" })]
+        public void FindsDependencies(string item, string[] dependencies)
+        {
+            Assert.That(_dependencies.For(item), Is.EqualTo(dependencies));
+        }
+    }
+}
---
+++ TransitiveDependenciesKata/Dependencies.cs
@@ -0,0 +1,14 @@
+﻿namespace TransitiveDependenciesKata
+{
+    public class Dependencies
+    {
+        public void AddDirect(string item, string[] dependencies)
+        {
+        }
+
+        public string[] For(string item)
+        {
+            return null;
+        }
+    }
+}
